08134d8f5aabad3cb88f42f0de0a376a8049e594
mirato12251999-blip <mirato12251999@gmail.com>
2025-12-18 11:59:27 -0800

    feature: change

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import { SquareChevronRight } from 'lucide-react';
+
 import styles from './task.module.css';
 
 export const Task = () => {
+    const [isShow, setShow] = useState(false);
+
+    const handleTask = () => {
+        setShow(!isShow);
+    }
+
     return (
-        <div className={styles["task"]}>
-            Task Component
+        <div className={styles["task"]} onClick={handleTask}>
+            <div className={styles["header"]}>
+                {
+                    isShow ? <SquareChevronRight className={styles["iconRotated"]} /> : <SquareChevronRight className={styles["icon"]} />
+                }
+                <p>Task Title</p>
+            </div>
+            {
+                isShow && (
+                    <div className={styles["details"]}>
+                        <p>Task details go here. This section is visible when isShow is true.</p>
+                    </div>
+                )
+            }
         </div>
     );
 };
